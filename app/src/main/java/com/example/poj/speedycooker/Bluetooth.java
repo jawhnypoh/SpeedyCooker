91874dcee47d3d7fbbccd8995e271da3bd33cccd
jawhnypoh <po.johnny@gmail.com>
2019-01-19 14:48:59 -0800

    Adding Bluetooth.java file

---
+++ app/src/main/java/com/example/poj/speedycooker/Bluetooth.java
@@ -0,0 +1,8 @@
+package com.example.poj.speedycooker;
+
+public class Bluetooth {
+    private String DEVICE_NAME = "HC-05"; // Figure out bluetooth device name
+    private String DEVICE_UUID;
+
+    
+}
