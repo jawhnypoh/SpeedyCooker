45f20cea1aca6c427a09d40a6fcdd9af5ae87e52
Stephen <oh.stephen98@gmail.com>
2019-01-19 12:23:51 -0800

    Updating files

--- app/src/main/java/com/example/poj/speedycooker/MainActivity.java
+++ app/src/main/java/com/example/poj/speedycooker/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.poj.speedycooker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity: ";
+
+    private TextView tempText, timeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tempText = (TextView)findViewById(R.id.temp_text);
+        timeText = (TextView)findViewById(R.id.time_text);
     }
 }
